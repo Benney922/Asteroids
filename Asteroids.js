56a8e96f5eff0482a2ba1ceec92e54657eae8e25
Benney922 <81079468+Benney922@users.noreply.github.com>
2021-04-22 14:10:28 +0200

    færdig med asteroiderne!!!

--- Asteroids.js
+++ Asteroids.js
@@ -5,11 +5,13 @@ class Asteroide {
     this.t2 = random(15,20);
     this.t3 = random(15,20);
     this.t4 = random(15,20);
+    this.t5 = random(5,10);
+    this.t6 = random(5,10);
+    this.t7 = random(25,35)
   
     this.r = 20;
     this.pos = createVector(random(width),random(height));
     this.vel = p5.Vector.random2D();
-    this.vel.mult(random(1,1.2));
   }
   
   update(){ 
@@ -27,7 +29,6 @@ class Asteroide {
     this.pos.y=height;
      }
     if (mouseX > this.pos.x-20 && mouseX < this.pos.x+20 && mouseY > this.pos.y-20 && mouseY < this.pos.y+20) {
-      print("1")
     }
    
   }
@@ -36,13 +37,15 @@ class Asteroide {
     strokeWeight(1);
     noFill();
     //ellipse(this.pos.x,this.pos.y,this.r,this.r);
-    beginShape();//Fået dem alle til at være random, lav nogle flere vertexer.
-    vertex(this.pos.x-this.t1,this.pos.y-this.t2);
-    vertex(this.pos.x-this.t3,this.pos.y+this.t4);
-    vertex(this.pos.x+this.t2,this.pos.y+this.t3);
-    vertex(this.pos.x+this.t4,this.pos.y-this.t1);
-    
-    
+    beginShape();
+     vertex(this.pos.x-this.t1,this.pos.y-this.t2); //venstre top
+    vertex(this.pos.x-this.t7,this.pos.y-this.t6);
+    vertex(this.pos.x-this.t3,this.pos.y+this.t4); //venstre bund
+    vertex(this.pos.x-this.t5,this.pos.y+this.t1);
+    vertex(this.pos.x+this.t2,this.pos.y+this.t3); //højre bund
+    vertex(this.pos.x+this.t6,this.pos.y+this.t6);
+    vertex(this.pos.x+this.t4,this.pos.y-this.t1); //højre top
+    vertex(this.pos.x,this.pos.y-this.t2); 
     endShape(CLOSE);
   }
   
