ccfe29b25aa7a132fd0b95e5f099f38daacbb300
Benney922 <81079468+Benney922@users.noreply.github.com>
2021-04-26 12:44:30 +0200

    hitbox, og forsøg på fjernelse af asteroide

--- Asteroids.js
+++ Asteroids.js
@@ -1,17 +1,17 @@
 class Asteroide {
   constructor() { 
   
-    this.t1 = random(15,20);
-    this.t2 = random(15,20);
+    this.t1 = random(15,30);
+    this.t2 = random(10,25);
     this.t3 = random(15,20);
-    this.t4 = random(15,20);
+    this.t4 = random(10,22);
     this.t5 = random(5,10);
     this.t6 = random(5,10);
     this.t7 = random(25,35)
-  
     this.r = 20;
     this.pos = createVector(random(width),random(height));
     this.vel = p5.Vector.random2D();
+    
   }
   
   update(){ 
@@ -28,7 +28,11 @@ class Asteroide {
   if(this.pos.y<0){
     this.pos.y=height;
      }
-    if (mouseX > this.pos.x-20 && mouseX < this.pos.x+20 && mouseY > this.pos.y-20 && mouseY < this.pos.y+20) {
+  
+  }
+  
+  hitbox(){
+    if (mouseX > this.pos.x-this.r && mouseX < this.pos.x+this.r && mouseY > this.pos.y-this.r && mouseY < this.pos.y+this.r) {
     this.t1 = this.t1/2;
     this.t2 = this.t2/2;
     this.t3 = this.t3/2;
@@ -37,8 +41,14 @@ class Asteroide {
     this.t6 = this.t6/2;
     this.t7 = this.t7/2;
     this.r = this.r/2;
+
+   }
+    if(this.r<4){
+    let newA = [];
+    newA[0] = new Asteroide();
+    newA[1] = new Asteroide();
+    return newA;
     }
-   
   }
   
   show(){ 
@@ -46,7 +56,7 @@ class Asteroide {
     noFill();
     //ellipse(this.pos.x,this.pos.y,this.r,this.r);
     beginShape();
-     vertex(this.pos.x-this.t1,this.pos.y-this.t2); //venstre top
+    vertex(this.pos.x-this.t1,this.pos.y-this.t2); //venstre top
     vertex(this.pos.x-this.t7,this.pos.y-this.t6);
     vertex(this.pos.x-this.t3,this.pos.y+this.t4); //venstre bund
     vertex(this.pos.x-this.t5,this.pos.y+this.t1);
